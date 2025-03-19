aed568d6aa3af3f6574c4520d6da7be4122247e3
mi1128h <mihyeon001128@gmail.com>
2025-03-19 23:32:34 +0900

    Chore: Update BlueMary BMP
    프레임당 사이즈 200x200
    ExtractGIF2.exe로 이미지 뽑고 y위치만 vs에서 조정
    
    ExtractGIF2.exe -> https://www.notion.so/ExtractGIF-1bb220c1e930806bab9ace37b734fd03?pvs=4

--- 250304_WinAPI/BlueMary.cpp
+++ 250304_WinAPI/BlueMary.cpp
@@ -4,7 +4,7 @@
 
 void BlueMary::Init()
 {
-	position = { 0,0 };
+	position = { 100,300 };
 	speed = 100;
 	dx = 0.0f;
 	dy = 0.0f;
@@ -14,49 +14,49 @@ void BlueMary::Init()
 	for (int i = 0; i < State::Statelength; ++i) vImages[i] = {};
 
 	Image* idleImages = new Image();
-	if (FAILED(idleImages->Init(L"Image/BlueMary/bluemary_stance.bmp", 840, 123, 12, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(idleImages->Init(L"Image/BlueMary/bluemary_stance.bmp", 200 * 12, 200, 12, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_stance 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::Idle].push_back(idleImages);
 	animTime[State::Idle] = 1.0f;
 
 	Image* walkImages = new Image();
-	if (FAILED(walkImages->Init(L"Image/BlueMary/bluemary_walk.bmp", 560, 107, 10, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(walkImages->Init(L"Image/BlueMary/bluemary_walk.bmp", 200 * 10, 200, 10, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_walk 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::Walk].push_back(walkImages);
 	animTime[State::Walk] = 1.0f;
 
 	Image* weakHandImages = new Image();
-	if (FAILED(weakHandImages->Init(L"Image/BlueMary/bluemary_weakhand.bmp", 340, 113, 4, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(weakHandImages->Init(L"Image/BlueMary/bluemary_weakhand.bmp", 200 * 4, 200, 4, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_weakhand 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::WeakHand].push_back(weakHandImages);
 	animTime[State::WeakHand] = 0.3f;
 
 	Image* strongHandImages = new Image();
-	if (FAILED(strongHandImages->Init(L"Image/BlueMary/bluemary_stronghand.bmp", 3168, 113, 22, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(strongHandImages->Init(L"Image/BlueMary/bluemary_stronghand.bmp", 200 * 22, 200, 22, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_stronghand 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::StrongHand].push_back(strongHandImages);
 	animTime[State::StrongHand] = 1.0f;
 
 	Image* weakFootImages = new Image();
-	if (FAILED(weakFootImages->Init(L"Image/BlueMary/bluemary_weakfoot.bmp", 891, 112, 9, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(weakFootImages->Init(L"Image/BlueMary/bluemary_weakfoot.bmp", 200 * 9, 200, 9, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_weakfoot 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::WeakFoot].push_back(weakFootImages);
 	animTime[State::WeakFoot] = 0.7f;
 
 	Image* strongFootImages = new Image();
-	if (FAILED(strongFootImages->Init(L"Image/BlueMary/bluemary_strongfoot.bmp", 1548, 115, 12, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(strongFootImages->Init(L"Image/BlueMary/bluemary_strongfoot.bmp", 200 * 12, 200, 12, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_strongfoot 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::StrongFoot].push_back(strongFootImages);
 	animTime[State::StrongFoot] = 1.0f;
 
 	Image* deadImages = new Image();
-	if (FAILED(deadImages->Init(L"Image/BlueMary/bluemary_fall.bmp", 4000, 128, 20, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(deadImages->Init(L"Image/BlueMary/bluemary_fall.bmp", 200 * 20, 200, 20, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_fall 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::Dead].push_back(deadImages);
