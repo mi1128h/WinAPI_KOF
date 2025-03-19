6561ccdb7f3cae4bf20d8bc0acf5713e78737044
eviloliver <evil_oliver@kakao.com>
2025-03-19 23:09:35 +0900

    WIP
    김갑환 에셋 수정

--- 250304_WinAPI/Kim.cpp
+++ 250304_WinAPI/Kim.cpp
@@ -13,7 +13,7 @@ void Kim::Init()
 	for (int i = 0; i < State::Statelength; ++i) vImages[i] = {};
 
 	Image* idleImages = new Image();
-	if (FAILED(idleImages->Init(L"Image/Kim/kim_idle.bmp", 256 * 4, 256, 4, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(idleImages->Init(L"Image/Kim/kim_stance.bmp", 256 * 4, 256, 4, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_stance 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::Idle].push_back(idleImages);
@@ -25,25 +25,25 @@ void Kim::Init()
 	vImages[State::Walk].push_back(walkImages);
 
 	Image* weakHandImages = new Image();
-	if (FAILED(weakHandImages->Init(L"Image/Kim/kim_AAttack.bmp", 256 * 3, 256, 3, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(weakHandImages->Init(L"Image/Kim/kim_weakhand.bmp", 256 * 3, 256, 3, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_weakhand 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::WeakHand].push_back(weakHandImages);
 
 	Image* strongHandImages = new Image();
-	if (FAILED(strongHandImages->Init(L"Image/Kim/kim_CAttack.bmp", 256 * 5, 256, 5, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(strongHandImages->Init(L"Image/Kim/kim_stronghand.bmp", 256 * 5, 256, 5, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_stronghand 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::StrongHand].push_back(strongHandImages);
 
 	Image* weakFootImages = new Image();
-	if (FAILED(weakFootImages->Init(L"Image/Kim/kim_BAttack.bmp", 256 * 5, 256, 5, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(weakFootImages->Init(L"Image/Kim/kim_weakfoot.bmp", 256 * 5, 256, 5, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_weakfoot 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::WeakFoot].push_back(weakFootImages);
 
 	Image* strongFootImages = new Image();
-	if (FAILED(strongFootImages->Init(L"Image/Kim/kim_DAttack.bmp", 256 * 5, 256, 5, 1, true, RGB(255, 0, 255)))) {
+	if (FAILED(strongFootImages->Init(L"Image/Kim/kim_strongfoot.bmp", 256 * 5, 256, 5, 1, true, RGB(255, 0, 255)))) {
 		MessageBox(g_hWnd, L"bluemary_strongfoot 파일 로드에 실패", L"경고", MB_OK);
 	}
 	vImages[State::StrongFoot].push_back(strongFootImages);
