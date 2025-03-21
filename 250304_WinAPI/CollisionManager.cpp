2073e40f8d17830221cb76467e4a2606e7ca93bd
ShinDongmin <59022397+eviloliver@users.noreply.github.com>
2025-03-21 10:54:09 +0900

    Update CollisionManager.cpp

--- 250304_WinAPI/CollisionManager.cpp
+++ 250304_WinAPI/CollisionManager.cpp
@@ -23,20 +23,31 @@ void CollisionManager::CheckHit(AnimCharacter* attacker, AnimCharacter* defender
 			if (attacker->GetState() == State::WeakFoot || attacker->GetState() == State::WeakHand)
 			{
 				// 약한 공격이면 attakcer->GetAttackValue()
-				defender->SetHp(defender->GetHp() - 1);
-				defender->SetState(State::WeakDamaged);
+
+				if (defender->GetState() == State::Defend)
+				{
+					defender->SetHp(defender->GetHp() - 0.5f);
+				}
+				else
+				{
+					defender->SetHp(defender->GetHp() - 1.f);
+					defender->SetState(State::WeakDamaged);
+				}
 			}
 			else if (attacker->GetState() == State::StrongFoot || attacker->GetState() == State::StrongHand)
 			{
 				// 강한 공격이면 attakcer->GetAttackValue() * 2
-				defender->SetHp(defender->GetHp() - 2);
-				defender->SetState(State::StrongDamaged);
+				if (defender->GetState() == State::Defend)
+				{
+					defender->SetHp(defender->GetHp() - 1.f);
+				}
+				else
+				{
+					defender->SetHp(defender->GetHp() - 2.f);
+					defender->SetState(State::StrongDamaged);
+				}
 			}
-			else if (defender->GetState() == State::Dead)
-			{
-				// TODO : Change to State Guard
-				defender->SetHp(defender->GetHp() - 0.5);
-			}
+			
 			attacker->SetIsSuccessHit(true);
 		}
 	}
